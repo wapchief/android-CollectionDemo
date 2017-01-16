c2ac22da0a25d93cba67900e064d44a55a9d8685
wapchief <wapchief@qq.com>
2017-01-16 10:25:59 +0800

    Regular updates

--- app/src/main/java/wapchief/com/collectiondemo/DividItemDecoration.java
+++ app/src/main/java/wapchief/com/collectiondemo/DividItemDecoration.java
@@ -43,7 +43,6 @@ public class DividItemDecoration extends RecyclerView.ItemDecoration{
 
     @Override
     public void onDraw(Canvas c, RecyclerView parent) {
-        Log.v("recyclerview - itemdecoration", "onDraw()");
 
         if (mOrientation == VERTICAL_LIST) {
             drawVertical(c, parent);
