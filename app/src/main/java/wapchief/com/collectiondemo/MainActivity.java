72b928813a5fd5a01ecf335f3778285ce06240e9
wapchief <wapchief@qq.com>
2017-02-17 10:44:18 +0800

    Commit单击返回，双击退出

--- app/src/main/java/wapchief/com/collectiondemo/MainActivity.java
+++ app/src/main/java/wapchief/com/collectiondemo/MainActivity.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -149,5 +153,43 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
 
+    }
+
+    /**
+     * 单击回退
+     * @param keyCode
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+     if (keyCode==KeyEvent.KEYCODE_BACK){
+            exitBy2Click();
+        }
+        return false;
+    }
+
+    /**
+     * 双击退出
+     */
+    private static Boolean isExit = false;
+
+    private void exitBy2Click() {
+        Timer tExit = null;
+        if (isExit == false) {
+            isExit = true; // 准备退出
+            Toast.makeText(this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+            tExit = new Timer();
+            tExit.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    isExit = false; // 取消退出
+                }
+            }, 2000); // 如果2秒钟内没有按下返回键，则启动定时器取消掉刚才执行的任务
+
+        } else {
+            finish();
+            System.exit(0);
+        }
     }
 }
